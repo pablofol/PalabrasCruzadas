7566d25b1ff757a07884abf1e1193ea6788ac588
pablofol <pfolgq00@estudiantes.unileon.es>
2013-12-04 22:44:54 +0100

    Cambiado todo a BackTracking (no funciona)

--- src/CrucePalabras.java
+++ src/CrucePalabras.java
@@ -1,27 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-
 
 
 
 
 public class CrucePalabras {
-	static final int MAX = 500;						//máximo tamaño del tablero
 	
 	
-	static class Estado{
-	    int x , y;								// Fila, columna y distancia
-		String d;
-	    
-	    public Estado( int x1, int y1 , String d1){
-	        this.x = x1;
-	        this.y = y1;
-	        this.d = d1;
-	    }
-	}
 
 	public static void main(String[] args) throws IOException {
 		
@@ -60,79 +46,50 @@ public class CrucePalabras {
 					System.out.print(" "+tabla[j][k]);
 				}
 			}
-		
-		System.out.println(resultado(tabla,tamano));
+		System.out.println();
+		System.out.println(recorrer(tabla,0,0,tamano,tabla[0][0]));
 			
 		
 		}//fin for casos
 	}//fin main
 	
 	
-	private static String resultado (String[][] tabla, int tamano){
+	
 		
-			boolean visitado[][] = new boolean[ MAX ][ MAX ];	//array de estados visitados
-			int x=0,y=0;
-			Queue<Estado> Q = new LinkedList<Estado>();			//Cola de todos los posibles Estados por los que se pase para llegar al destino
-			Q.add( new Estado( x , y , "" ) );					//Insertamos el estado inicial en la Cola con distnacia 0.
-			
-			int dx[  ] = { 0 , 1 , 1 , 1 , 0 , -1, -1, -1};	//incremento en coordenada x
-			int dy[  ] = { 1 , 1 , 0 ,-1 ,-1 , -1 , 0 , 1};		//incremento en coordenada y
-			
-			int nx , ny;
-			
-			
-			while( !Q.isEmpty() ){							//Mientras cola no este vacia
-				Estado actual = Q.remove();					//Obtengo de la cola el estado actual, en un comienzo será el inicial
-				String d=actual.d;
-				//Si llega al destino
-				visitado[ actual.x ][ actual.y ] = true;	//Marco como visitado dicho estado para no volver a recorrerlo
-				
-				for( int i = 0 ; i < 8 ; i++ ){				//Recorremos hasta 8 porque tenemos 8 posibles adyacentes
-					nx = dx[ i ] + actual.x;				//nx y ny tendran la coordenada adyacente
-					ny = dy[ i ] + actual.y;				//ejemplo en i=0 y actual (3,4) -> 3+dx[0]=3+1=4, 4+dy[0]=4+2=6, nueva coordenada (4,6)
-			        										//aqui comprobamos que la coordenada adyacente no sobrepase las dimensiones del tablero
-			        										//ademas comprobamos que no este visitado
-					if( nx >= 0 && nx < tamano && ny >= 0 && ny < tamano && !visitado[ nx ][ ny ] && 0<actual.d.compareTo(d)){
-						Q.add( new Estado( nx , ny , d+actual.d ) ); //agregamos estado adyacente aumento en 1 la distancia recorrida
-						d=d+actual.d;
-					}
-					else System.out.print(d);
-					
-				}
-				return d;
-			}
-			
-			return null;
-		
-		
-/*
-		public static void main(String[] args) throws IOException {
-			
-			
-			InputStreamReader isr = new InputStreamReader(System.in);
-			BufferedReader br = new BufferedReader (isr);
-			String texto="";
-			texto = br.readLine();				//Leemos entrada
-			String[] entrada= texto.split(" ");
-			int tamano=Integer.parseInt(entrada[0]);		//Tamaño Tablero		
-			int salx=Integer.parseInt(entrada[1]);			//Coordenada X inicial
-			int saly=Integer.parseInt(entrada[2]);			//Coordenada Y inicial
-			int llex=Integer.parseInt(entrada[3]);			//Coordenada X final
-			int lley=Integer.parseInt(entrada[4]);			//Coordenada Y final
+		boolean existeSolucion=false;
+		int n=1;
 
+		private static String recorrer(String[][] tabla, int i, int j, int tamano, String d){		//función backtracking
+			//comprueba si es solución
+		   				
+			//marcar camino
 			
-			
-			int min = BFS( salx , saly , tamano , llex, lley );			//Llamada al metodo BFS
-			if( min == -1 ) System.out.print("0 -1");				//No hay solución posible
-			else System.out.println( min );		//Imprime resultado
-			
-		}
-*/
+			//imprimir(laberinto);
+
+			if(i-1>=0 && i-1<tamano && tabla[i-1][j].charAt(0)>d.charAt(d.length()-1)){
+				
+				d=d+(recorrer(tabla, i-1, j, tamano,d));
+			}
+
+			if(i-1>=0 && i-1<tamano && tabla[i][j+1].charAt(0)>d.charAt(d.length()-1)){
+				
+				d=d+(recorrer(tabla, i-1, j, tamano,d));
+			}
+
+			if(i-1>=0 && i-1<tamano && tabla[i+1][j].charAt(0)>d.charAt(d.length()-1)){
+				
+				d=d+(recorrer(tabla, i-1, j, tamano,d));
+			}
+
+			if(i-1>=0 && i-1<tamano && tabla[i][j-1].charAt(0)>d.charAt(d.length()-1)){
+				
+				d=d+(recorrer(tabla, i-1, j, tamano,d));
+			}
+			return d;
+
+			//desmarcar camino
+		}	
 		
-		
-		
-		
-		
-	}
+	
 }//fin clase
 
